7127424a820b7e1a9cea3eecea8106c9e7c593b7
PblCbGomel <pblcb.gomel@gmail.com>
2023-01-06 22:45:25 +0300

    feat: add functional of i18n in settings

--- src/app/header/settings/settings.component.ts
+++ src/app/header/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { ThemeChangeServiceService } from 'src/app/services/theme-change-service.service';
 
 @Component({
@@ -6,17 +7,34 @@ import { ThemeChangeServiceService } from 'src/app/services/theme-change-service
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
   @ViewChild('numberMenu') numberMenu: ElementRef | undefined;
 
   numberOfColors: String = 'filter_3';
-  numberItems = [3, 4, 5, 6];
+  numberItems: Number[] = [3, 4, 5, 6];
+  languageItems: String[] = ['en-US', 'ru-RU'];
   theme: String = 'brightness_7';
 
-  constructor(private changeThemeS: ThemeChangeServiceService) {}
+  constructor(
+    private changeThemeS: ThemeChangeServiceService,
+    private translate: TranslateService
+  ) {}
 
-  numberMenuClick(item: any) {
-    this.numberOfColors = 'filter_' + item;
+  ngOnInit(): void {
+    this.translate.setDefaultLang(localStorage.getItem('lang') || 'en-US');
+    this.translate.use(localStorage.getItem('lang') || 'en-US');
+  }
+
+  numberMenuClick(num: Number) {
+    this.numberOfColors = 'filter_' + num;
+  }
+
+  languageMenuClick(language: String) {
+    if ((language = 'en-US')) {
+      this.translate.use('en-US');
+    } else {
+      this.translate.use('ru-RU');
+    }
   }
 
   changeTheme() {
