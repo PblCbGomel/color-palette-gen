6d9485418301257b91bace29e902ffdf4eb2a708
PblCbGomel <pblcb.gomel@gmail.com>
2023-01-12 14:09:59 +0300

    feat: add lock btn

--- src/app/main/color-block/color-block.component.ts
+++ src/app/main/color-block/color-block.component.ts
@@ -8,10 +8,22 @@ import { GenerateRandomColorService } from 'src/app/services/generate-random-col
 })
 export class ColorBlockComponent implements OnInit {
   selectedColor: String = '';
+  lockIcon = 'lock_open';
+  isLocked: boolean = false;
 
   constructor(private GenerateColorService: GenerateRandomColorService) {}
 
   ngOnInit(): void {
     this.selectedColor = this.GenerateColorService.generateColor();
+  }
+
+  changeLock(): void {
+    if (this.isLocked === true) {
+      this.isLocked = false;
+      this.lockIcon = 'lock_open';
+    } else {
+      this.isLocked = true;
+      this.lockIcon = 'lock';
+    }
   }
 }
